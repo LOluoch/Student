22c35a0932ac99fc841da84a6c9bffc40c130dff
LOluoch <lavineoluoch@gmail.com>
2015-09-09 12:02:14 +0000

    initial commit

---
+++ resources/views/trial/about.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE
+    html
+    >
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!-->
+<html>
+<!--<![endif]-->
+<head>
+    <meta
+        charset=
+        "UTF-8"
+        >
+    <meta
+        http-equiv=
+        "X-UA-Compatible"
+        content=
+        "IE=edge,chrome=1"
+        >
+    <title>
+        Akirachix | About
+    </title>
+    <meta
+        name=
+        "author"
+        content=
+        "chencha"
+        >
+    <meta
+        name=
+        "description"
+        content=
+        "About us page"
+        >
+    <meta
+        name=
+        "keywords"
+        content=
+        "home, akirachix, geek girl"
+        >
+</head>
+<body>
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</body>
+</html>
---
+++ resources/views/trial/blog.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE
+    html
+    >
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!-->
+<html>
+<!--<![endif]-->
+<head>
+    <meta
+        charset=
+        "UTF-8"
+        >
+    <meta
+        http-equiv=
+        "X-UA-Compatible"
+        content=
+        "IE=edge,chrome=1"
+        >
+    <title>
+        Akirachix | Blog
+    </title>
+    <meta
+        name=
+        "author"
+        content=
+        "chencha"
+        >
+    <meta
+        name=
+        "description"
+        content=
+        "Blog page"
+        >
+    <meta
+        name=
+        "keywords"
+        content=
+        "home, akirachix, geek girl"
+        >
+</head>
+<body>
+</body>
+</html>
